4134b0a9508a3a2b9becaa15d67d7652b7bf9757
Baeji77 <hahawjstk@gmail.com>
2018-06-28 19:06:25 +0900

    2.4.2 forward

---
+++ firstWeb/src/example/nextSevlet.java
@@ -0,0 +1,48 @@
+package example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class nextSevlet
+ */
+@WebServlet("/next")
+public class nextSevlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public nextSevlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>form</title></head>");
+        out.println("<body>");
+        int dice = (Integer)request.getAttribute("dice");
+        out.println("dice : " + dice);
+        for(int i = 0 ; i < dice ; i++) {
+        		out.println("Hello<br>");
+        }
+        
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+}
+
