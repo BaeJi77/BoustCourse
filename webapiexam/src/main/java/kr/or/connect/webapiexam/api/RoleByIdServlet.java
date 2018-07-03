9997900e5e944539b939d2b6b20eada69e6176db
{Baeji77} <{hahawjstk@gmail.com}>
2018-07-03 15:22:47 +0900

    maven를 통한 webApplication 동작

---
+++ webapiexam/src/main/java/kr/or/connect/webapiexam/api/RoleByIdServlet.java
@@ -0,0 +1,46 @@
+package kr.or.connect.webapiexam.api;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.connet.jdbcexam1.dao.roleDao;
+import kr.or.connet.jdbcexam1.dto.role;
+
+/**
+ * Servlet implementation class RoleByIdServlet
+ */
+@WebServlet("/roles/*")
+public class RoleByIdServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RoleByIdServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("application/json");
+		
+		String path = request.getPathInfo();
+		String[] split = path.split("/");
+		String getId = split[1];
+		int realId = Integer.parseInt(getId);
+		
+		roleDao dao = new roleDao();
+
+		role role = dao.getRole(realId);
+	}
+
+}
